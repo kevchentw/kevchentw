02f31fcfcbb9a367743443dd33428a61a5614077
Kevin Chen <me@kevchentw.com>
2019-12-01 22:43:54 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `Kevin Chen`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     navigation: [
       {
@@ -14,6 +15,10 @@ module.exports = {
         title: `About`,
         slug: `/about`,
       },
+      {
+        title: `Publications`,
+        slug: `/publications`,
+      }
     ],
   },
   plugins: [
